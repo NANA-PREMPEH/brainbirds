6ee2cacd628d7fcccf1d5e76f61d555c1cd34617
unknown <HP@DESKTOP-PREMPEH.highways.ho.gh>
2022-03-20 02:19:19 +0000

    Updated

--- bba/main/routes.py
+++ bba/main/routes.py
@@ -8,10 +8,10 @@ main = Blueprint('main', __name__)
 @main.route("/home")
 def home():
     #displays the posts saved in the database at the home page always
-    page = request.args.get('page', 1, type=int)
+    # page = request.args.get('page', 1, type=int)
     #paginate is the number of page to display on the web page
     #post.date_posted.desc is the arrangement for the post on th page
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    # posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     #return render_template('homepage/homepage_standard.html')
     return render_template('main/home.html')
 
